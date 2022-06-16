19ab76fd1775b1de62f34b89ed9b7423f7c88c0b
Hanro <hlareman2@gmail.com>
2022-06-16 17:50:12 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import cluster from "cluster";
 
 import express from "express";
-import { copyFileSync, createReadStream, existsSync, fstatSync, lstatSync, mkdirSync, readdirSync, readFileSync, symlinkSync, unlinkSync, writeFileSync } from "fs";
+import { createReadStream, existsSync } from "fs";
+import { mkdir, writeFile, lstat, unlink, symlink, copyFile, readFile, readdir } from "fs/promises";
 import { cpus } from "os";
 import { join, resolve } from "path";
 export type fileType = "file" | "dir" | "link" | "sys";
@@ -22,42 +23,42 @@ if (cluster.isPrimary) {
         cluster.fork();
     });
     if (!existsSync("download")) {
-        mkdirSync("download")
+        await mkdir("download")
     }
-    writeFileSync(join("download", "lastReboot.txt"), new Date().toISOString());
+    await writeFile(join("download", "lastReboot.txt"), new Date().toISOString());
     let src = join("download", "source");
-    const srcStat = lstatSync(src)
+    const srcStat = await lstat(src)
     if (srcStat.isSymbolicLink()) {
-        unlinkSync(src)
-        symlinkSync(resolve("src"), src, "junction");
+        await unlink(src)
+        await symlink(resolve("src"), src, "junction");
     }
-    copyFileSync("LICENSE", join("download", "LICENSE"))
+    copyFile("LICENSE", join("download", "LICENSE"))
     if (!existsSync("settings.json")) {
-        writeFileSync("settings.json", JSON.stringify({ port: 3000 }))
+        await writeFile("settings.json", JSON.stringify({ port: 3000 }))
     }
 
     cpus().forEach(e => {
         cluster.fork()
     })
 } else {
-    const settings = JSON.parse(readFileSync("settings.json").toString())
+    const settings = JSON.parse((await readFile("settings.json")).toString())
     const app = express();
     const port = settings.port;
 
-    app.use("/favicon.ico", (req, res) => {
-        res.status(200).send(readFileSync("favicon.ico")).end();
+    app.use("/favicon.ico", async (req, res) => {
+        res.status(200).send(await readFile("favicon.ico")).end();
     })
     app.use("/script", express.static("dist/web"));
     app.get("/api/back", (req, res) => {
         res.redirect(settings.main || "/")
     })
-    app.use((req, res) => {
+    app.use(async (req, res) => {
         console.log(req.headers)
         if (req.url.includes(".."))
             return res.status(403).end();
         const dir = join(...["download", ...req.url.split("/")]);
         if (!existsSync(dir)) return res.status(404);
-        if (lstatSync(dir).isFile()) {
+        if ((await lstat(dir)).isFile()) {
             res.set('Cache-control', `public, max-age=${3600 * 24}`)
             res.set('Content-Type", "application/octet-stream');
             res.attachment();
@@ -77,24 +78,25 @@ if (cluster.isPrimary) {
                 f.push('')
                 links.push({ name: "back", link: f.join("/"), type: "sys" })
 
-            } readdirSync(dir).forEach(e => {
+            }
+            const dirLS = (await readdir(dir))
+            for (let index = 0; index < dirLS.length; index++) {
+                const e = dirLS[index];
+
                 try {
                     const file = join(dir, e);
-
-                    const fstad = lstatSync(file)
+                    const fstad = await lstat(file)
                     let type: "file" | "dir" | "link" = fstad.isDirectory() ? "dir" : fstad.isSymbolicLink() ? "link" : "file"
-
-
                     links.push({ mod: fstad.mtime.getTime(), type, name: e, link: `${req.url}${req.url.endsWith("/") ? "" : "/"}${e}` })
                 } catch (e) {
 
                 }
-            })
+            }
             console.log(req.url)
             res.status(200).type("json").send(JSON.stringify(links))
 
         } else {
-            res.status(200).type("html").send(readFileSync('404.html')).end()
+            res.status(200).type("html").send(await readFile('404.html')).end()
         }
     })
 
