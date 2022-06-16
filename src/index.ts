3a95658fd19c26554038cc9c2cd20d028d2b408f
Hanro <hlareman2@gmail.com>
2022-06-16 16:45:31 +0200

    Initial upload

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import cluster from "cluster";
+
+import express from "express";
+import { copyFileSync, createReadStream, existsSync, fstatSync, lstatSync, mkdirSync, readdirSync, readFileSync, symlinkSync, unlinkSync, writeFileSync } from "fs";
+import { cpus } from "os";
+import { join, resolve } from "path";
+
+export type file = {
+    name: string,
+    link: string,
+    type: "file" | "dir" | "link",
+    mod?: number
+}
+
+if (cluster.isPrimary) {
+    console.log(`Number of CPUs is ${cpus().length}`);
+    console.log(`Master ${process.pid} is running`);
+    cpus().forEach(e => {
+        cluster.fork()
+    })
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died with code ${code}`);
+        console.log("Let's fork another worker!");
+        cluster.fork();
+    });
+    if (!existsSync("download")) {
+        mkdirSync("download")
+    }
+    writeFileSync(join("download", "lastReboot.txt"), new Date().toISOString());
+    let src = join("download", "source");
+    const srcStat = lstatSync(src)
+    if (srcStat.isSymbolicLink()) {
+        unlinkSync(src)
+        symlinkSync(resolve("src"), src, "junction");
+    }
+    copyFileSync("LICENSE", join("download", "LICENSE"))
+} else {
+    const app = express();
+    const port = 3000;
+
+    app.use("/favicon.ico", (req, res) => {
+        res.status(200).send(readFileSync("favicon.ico")).end();
+    })
+    app.use("/script", express.static("dist/web"));
+    app.use((req, res) => {
+        console.log(req.headers)
+        if (req.url.includes(".."))
+            return res.status(403).end();
+        const dir = join(...["download", ...req.url.split("/")]);
+        if (!existsSync(dir)) return res.status(404);
+        if (lstatSync(dir).isFile()) {
+            res.set('Cache-control', `public, max-age=${3600 * 24}`)
+            res.set('Content-Type", "application/octet-stream');
+            res.attachment();
+            const stream = createReadStream(dir);
+            stream.pipe(res, { end: true })
+            return
+        }
+        res.set('Cache-control', `no-cache`)
+        if (req.headers["content-type"] == "json") {
+
+            console.log("html")
+            let f = req.url.split('/')
+
+            const links: Array<file> = [];
+
+            if (f.pop() || f.pop()) {
+                f.push('')
+                links.push({ name: "back", link: f.join("/"), type: "dir" })
+
+            } readdirSync(dir).forEach(e => {
+                try {
+                    const file = join(dir, e);
+
+                    const fstad = lstatSync(file)
+                    let type: "file" | "dir" | "link" = fstad.isDirectory() ? "dir" : fstad.isSymbolicLink() ? "link" : "file"
+
+
+                    links.push({ mod: fstad.mtime.getTime(), type, name: e, link: `${req.url}${req.url.endsWith("/") ? "" : "/"}${e}` })
+                } catch (e) {
+
+                }
+            })
+            console.log(req.url)
+            res.status(200).type("json").send(JSON.stringify(links))
+
+        } else {
+            res.status(200).type("html").send(readFileSync('404.html')).end()
+        }
+    })
+
+    app.listen(port);
+    console.log(`Client ${process.pid} is running`);
+}
---
+++ src/web/files.ts
@@ -0,0 +1,53 @@
+import type { file } from "../index.js";
+export const blank = "";
+const r = await fetch(window.location.pathname, {method:"POST", headers: { "content-type": "json" } })
+const json: Array<file> = await r.json();
+const fhandler = document.getElementById("fhandler") as HTMLSpanElement
+fhandler.innerText = `<${window.location.pathname}>`
+const table = document.getElementById("content") as HTMLTableElement;
+let trh = document.createElement("tr");
+let th1 = document.createElement("th");
+th1.innerText = "Name"
+trh.appendChild(th1)
+let th2 = document.createElement("th");
+th2.innerText = "Type"
+trh.appendChild(th2)
+let th3 = document.createElement("th");
+th3.innerText = "Date modified"
+trh.appendChild(th3)
+
+table.appendChild(trh);
+
+
+console.log(json)
+json.forEach(e => {
+    const trd = document.createElement("tr");
+    if (e.type == "dir") {
+        trd.onclick = () => {
+            window.location.replace(e.link);
+        }
+    } else
+        trd.onclick = () => window.open(e.link);
+    let td1 = document.createElement("td");
+    td1.innerText = e.name;
+    trd.appendChild(td1)
+
+    let td2 = document.createElement("td");
+    td2.innerText = e.type;
+    trd.appendChild(td2)
+
+    let td3 = document.createElement("td");
+    if (e.mod) {
+        const d = new Date(e.mod);
+        let options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric', "hour": "2-digit", "minute": "2-digit" };
+        td3.innerText = d.toLocaleString(Intl.DateTimeFormat().resolvedOptions().locale, options)
+    } else {
+        td3.innerText = "";
+    }
+
+    trd.appendChild(td3)
+
+    table.appendChild(trd)
+})
+
+
